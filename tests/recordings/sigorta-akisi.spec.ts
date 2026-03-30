aa38d57a4b1a28c7057899330bc3beb5d7c2dfc6
yasindeger <yasin.deger@hesapkurdu.com>
2026-03-30 16:34:16 +0300

    testflow: update playwright specs

--- tests/recordings/sigorta-akisi.spec.ts
+++ tests/recordings/sigorta-akisi.spec.ts
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test'
 
 test('sigorta akışı', async ({ page }) => {
   await page.goto('https://www.hesapkurdu.com/');
-  await expect(page.getByRole('img', { name: 'percent' }).nth(0)).toBeVisible();
   await expect(page.getByRole('button', { name: 'İhtiyaç Kredisi' }).nth(0)).toBeVisible();
   await expect(page.locator('h1')).toContainText('En Avantajlı Finansal Fırsatlar');
   await page.getByRole('button', { name: 'Sigorta' }).nth(0).click();
