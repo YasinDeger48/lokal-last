103fb1dfe5da5a332726d2fabb598e128e5d4401
Yasin Değer <87366280+YasinDeger48@users.noreply.github.com>
2026-04-06 12:45:00 +0300

    changed a couple of scnearios

--- tests/recordings/sigorta-akisi.spec.ts
+++ tests/recordings/sigorta-akisi.spec.ts
@@ -6,3 +6,15 @@ test('sigorta akışı', async ({ page }) => {
   await page.getByRole('button', { name: 'Sigorta' }).nth(0).click();
   await expect(page.locator('[href="https://www.koalay.com/"]').nth(0)).toBeVisible();
 })
+
+test('homepage has a title', async ({ page }) => {
+  await page.goto('https://example.com')
+  await expect(page).toHaveTitle(/Example/)
+})
+
+test('sigorta akışı2', async ({ page }) => {
+  await page.goto('https://www.hesapkurdu.com/');
+  await expect(page.getByRole('heading', { name: 'En Avantajlı Finansal Fırsatlar', exact: true }).nth(0)).toHaveText('En Avantajlı Finansal Fırsatlar');
+  await page.getByRole('button', { name: 'Sigorta' }).nth(0).click();
+  await expect(page.locator('[href="https://www.koalay.com/"]').nth(0)).toBeVisible();
+})
