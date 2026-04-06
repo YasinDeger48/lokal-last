956e1145722f8bb529faae7f004d6c6c07d744dd
Yasin Değer <87366280+YasinDeger48@users.noreply.github.com>
2026-04-06 13:49:21 +0300

    added new one

---
+++ tests/recordings/abc.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test'
+
+test('abc testing deneme', { tag: ['@smoke'] }, async ({ page }) => {
+  test.setTimeout(180000)
+  page.setDefaultTimeout(10000)
+  page.setDefaultNavigationTimeout(30000)
+
+  await page.goto('https://www.hesapkurdu.com/');
+  await page.waitForLoadState('load', { timeout: 30000 })
+  await expect(page.getByText('En Avantajlı Finansal Fırsatlar', { exact: true })).toBeVisible();
+})
